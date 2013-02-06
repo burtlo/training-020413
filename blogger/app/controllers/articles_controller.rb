a0a029ee6bc1f06cabe40c3ac3804c3c6e724081
Franklin Webber <franklin.webber@gmail.com>
2013-02-06 11:59:25 -0800

    Blogger through Iteration 1

---
+++ blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    id = params[:id]
+    @article = Article.find(id)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create params[:article]
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    flash[:message] = t("article.update.success")
+    redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+end
---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '9d5ca0fea2d85b9086a34edbf359f037aeec0fb1e54f5b3539f44a69e1b37fced8e7e9eb58c54fc740ccac72f8661fe9cd75892bb1514ea91d7843950a5fe8f8'
