6da9f2a33f406752f8c4773f7d28500d600a5add
Franklin Webber <franklin.webber@gmail.com>
2013-02-07 14:32:43 -0800

    Tagging - handling duplication

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  attr_accessible :title, :body, :image
+  attr_accessible :title, :body, :image, :tag_list
 
   validates :title, :body, presence: true
 
@@ -13,6 +13,23 @@ class Article < ActiveRecord::Base
 
   def tag_list
     tags.map { |tag| tag.name }.join(", ")
+  end
+
+  def tag_list=(value)
+    tag_names = split_and_clean_tag_strings(value)
+
+    tag_names.each do |tag_name|
+      unless tags.find { |tag| tag.name == tag_name }
+        tags << Tag.find_or_create_by_name(tag_name)
+      end
+    end
+
+  end
+
+  private
+
+  def split_and_clean_tag_strings(value)
+    value.to_s.split(",").map {|tag_name| tag_name.strip }.uniq
   end
 
 end
--- blogger/spec/models/article_spec.rb
+++ blogger/spec/models/article_spec.rb
@@ -14,6 +14,13 @@ describe Article do
       end
     end
 
+    context "when the article has no tags" do
+      let(:given) { [ ] }
+      let(:expected) { "" }
+
+      it_behaves_like "the correct tag list"
+    end
+
     context "when the article has one tag" do
       let(:given) { [ "First Tag" ] }
       let(:expected) { "First Tag" }
@@ -22,16 +29,66 @@ describe Article do
     end
 
     context "when the article has two tags" do
-
       let(:given) { [ "First Tag", "Second Tag" ] }
       let(:expected) { "First Tag, Second Tag" }
 
-      it "should return the two tags in the tag list" do
-        given.each {|tag_name| subject.tags.build name: tag_name }
-        subject.tag_list.should eq expected
+      it_behaves_like "the correct tag list"
+    end
+
+  end
+
+  describe "#tag_list=" do
+    context "when given an empty string" do
+      it "should have one tag" do
+        subject.tag_list = ""
+        expect(subject.tags.length).to eq 0
+      end
+    end
+    context "when given a string with one tag" do
+      it "should have one tag" do
+        subject.tag_list = "First Tag"
+        expect(subject.tags.length).to eq 1
+        expect(subject.tags.first.name).to eq "First Tag"
       end
     end
 
+    context "when the article already has the tag provided" do
+      it "should not add the new tag" do
+        subject.tags.build name: "First Tag"
+        subject.tag_list = "First Tag"
+        expect(subject.tags.length).to eq 1
+        expect(subject.tags.first.name).to eq "First Tag"
+      end
+    end
+
+    context "when a tag is already defined in the database" do
+      it "should not create a new tag with the same name" do
+        first_tag = Tag.create name: "First Tag"
+        subject.tag_list = "First Tag"
+        expect(subject.tags.first.name).to eq "First Tag"
+        expect(subject.tags.first).to eq first_tag
+      end
+    end
+
+
+
+    context "when given a string with two tag" do
+      it "should have two tags" do
+        subject.tag_list = "First Tag, Second Tag"
+        expect(subject.tags.length).to eq 2
+        expect(subject.tags.first.name).to eq "First Tag"
+        expect(subject.tags.last.name).to eq "Second Tag"
+      end
+      
+      context "when there are two similar tags" do
+        it "should have one tag" do
+          subject.tag_list = "First Tag, First Tag"
+          expect(subject.tags.length).to eq 1
+          expect(subject.tags.first.name).to eq "First Tag"
+        end
+      end
+      
+    end
   end
 
   it "should have comments" do
