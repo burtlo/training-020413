9fee161e676faff7c606b6f5944516ab1da25d95
Franklin Webber <franklin.webber@gmail.com>
2013-02-06 16:03:42 -0800

    Paperclip Gem Integration

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ActiveRecord::Base
-  attr_accessible :title, :body
+  attr_accessible :title, :body, :image
 
   validates :title, :body, presence: true
 
   has_many :comments
+  has_attached_file :image, styles: { 
+    thumbnail: "100x100>",
+    medium: "300x300>" }
+
 end
--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206210748) do
+ActiveRecord::Schema.define(:version => 20130206234904) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", :force => true do |t|
