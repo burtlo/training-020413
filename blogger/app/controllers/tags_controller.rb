8319cf7e64a5baf4d0a47889a3709d613838e3bd
Franklin Webber <franklin.webber@gmail.com>
2013-02-07 15:57:27 -0800

    Tags linked up on articles

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 class TagsController < ApplicationController
   def index
-
+    @tags = Tag.all
   end
 
   def show
-
+    @tag = Tag.find_by_name(params[:id]) || Tag.find(params[:id])
   end
 end
